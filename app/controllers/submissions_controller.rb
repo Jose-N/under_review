8bce65e534b2c9108eb960ca6c9949074a736826
Jose Naylor <jose.c.naylor@gmail.com>
2017-09-06 14:33:11 -0400

    created test spec for submitting a review, created routes, view, and modified controller to create submission form, added custom error message for null screenshot

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -2,4 +2,23 @@ class SubmissionsController < ApplicationController
   def index
     @submissions = Submission.all
   end
+
+  def new
+    @submission = Submission.new
+  end
+
+  def create
+    @submission = Submission.new(submission_params)
+    if @submission.save
+      redirect_to @submission,
+      notice: "This Jawn Has Been Saved"  
+    else
+      render :new
+    end
+  end
+
+  private
+    def submission_params
+      params.require(:submission).permit(:user_id, :title, :description, :url, :screenshot)
+    end
 end
