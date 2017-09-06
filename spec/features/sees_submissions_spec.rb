aed448e04539ba126b8f39c8c41122bf74fb64fd
Jose Naylor <jose.c.naylor@gmail.com>
2017-09-06 13:21:37 -0400

    added public images to gitignore, added flash notices to layout, created factory for submissions, added test image for submissions, updated sees_submissions_spec to use carrierwave and factorygirl

--- spec/features/sees_submissions_spec.rb
+++ spec/features/sees_submissions_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 feature "sees submissions" do
   scenario "see all the submissions" do
-    submission = Submission.create(title: "Not stunting in my Air Force Ones", user_id: 1)
-    another_submission = Submission.create(title: "This product caused my divorce", user_id: 1)
+    submission = FactoryGirl.create(:submission)
+    another_submission = FactoryGirl.create(:submission, :full)
 
     visit submissions_path
     expect(page).to have_content(submission.title)
