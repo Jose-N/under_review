8d6e9ee584e86b037e685cdc3dffa0441b79030c
Thomas Wilson <twilson.spqr@Sleipnir.local>
2017-09-07 11:24:00 -0400

    completed adding user sign up, all specs green

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root 'submissions#index'
 
   devise_for :users
-  resources :submissions, only: [:index]
+  resources :submissions, only: [:index, :show, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -9,18 +9,39 @@ feature 'sign up', %(
   scenario 'specifying valid and required information' do
     visit submissions_path
     click_link 'Sign Up'
+
     fill_in 'First Name', with: 'Jon'
     fill_in 'Last Name', with: 'Smith'
     fill_in 'Email', with: 'user@example.com'
     fill_in 'user_password', with: 'password'
     fill_in 'Password Confirmation', with: 'password'
+
     click_button 'Sign Up'
 
     expect(page).to have_content("You're in!")
     expect(page).to have_content("Sign Out")
   end
 
-  scenario 'required information is not supplied'
+  scenario 'required information is not supplied' do
+    visit new_user_registration_path
+    click_button 'Sign Up'
 
-  scenario 'password confirmation does not match confirmation'
+    expect(page).to have_content("can't be blank")
+    expect(page).to_not have_content("Sign Out")
+  end
+
+  scenario 'password confirmation does not match confirmation' do
+    visit new_user_registration_path
+
+    fill_in 'First Name', with: 'Jon'
+    fill_in 'Last Name', with: 'Smith'
+    fill_in 'Email', with: 'user@example.com'
+    fill_in 'user_password', with: 'password'
+    fill_in 'Password Confirmation', with: 'somethingDifferent'
+
+    click_button 'Sign Up'
+
+    expect(page).to have_content("doesn't match")
+    expect(page).to_not have_content("Sign Out")
+  end
 end
