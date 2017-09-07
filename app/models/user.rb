6611e5e15d2d50f7b397f7f82c7a3db70e74663c
Thomas Wilson <twilson.spqr@Sleipnir.local>
2017-09-07 14:33:49 -0400

    fixed code climate issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   validates_presence_of :first_name
   validates_presence_of :last_name
-validates :email, :presence => true, :format => { :with => /^([^@\s]+)@((?:[-a-z0-9]+.)+[a-z]{2,})$/i, :multiline => true }
+validates :email, :presence => true, :format => { :with => /\A^([^@\s]+)@((?:[-a-z0-9]+.)+[a-z]{2,})$\z/i, :multiline => true }
 
 
 
