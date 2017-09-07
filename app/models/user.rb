66455dc09b0ef69722e78aa74f2b425636f3ba1f
Thomas Wilson <twilson.spqr@Sleipnir.local>
2017-09-07 14:54:17 -0400

    edited in compliance with supervisor requests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   validates_presence_of :first_name
   validates_presence_of :last_name
-validates :email, :presence => true, :format => { :with => /\A^([^@\s]+)@((?:[-a-z0-9]+.)+[a-z]{2,})$\z/i, :multiline => true }
+validates :email, presence: true, format: { with: /\A^([^@\s]+)@((?:[-a-z0-9]+.)+[a-z]{2,})$\z/i, multiline: true }
 
 
 
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 end
