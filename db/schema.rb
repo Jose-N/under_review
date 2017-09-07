2601813bc00c49f7e96af6d44995caa24f2291ac
Thomas Wilson <twilson.spqr@Sleipnir.local>
2017-09-07 11:04:35 -0400

    succesfully merged in master

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20170906180055) do
-=======
-ActiveRecord::Schema.define(version: 20170906195651) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
