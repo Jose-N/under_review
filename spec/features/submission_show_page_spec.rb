f341c328baec0af885a7d23164632d653bab1e31
Kristen Lynne Jordan <kristlynne@Kristens-MacBook-Pro.local>
2017-09-07 11:20:12 -0400

    Updated submission show page to view ratings, updated spec to test for ratings, created factory for ratings

--- spec/features/submission_show_page_spec.rb
+++ spec/features/submission_show_page_spec.rb
@@ -15,7 +15,7 @@ feature "see show page for a submission" do
     full_submission = FactoryGirl.create(:submission, :full)
 
     visit submission_path(full_submission)
-    
+
     expect(page).to have_content(full_submission.title)
     expect(page).to have_content(full_submission.description)
     expect(page).to have_link("Original Review", href: full_submission.url)
@@ -31,5 +31,21 @@ feature "see show page for a submission" do
 
     visit submission_path(min_submission)
     expect(page).to have_content(first_comment.body)
+  end
+end
+
+
+feature "user can see ratings on show page" do
+  scenario "user goes to show page for a submission" do
+    min_submission = FactoryGirl.create(:submission)
+    first_comment = FactoryGirl.create(:comment, submission_id: min_submission.id)
+    first_rating = FactoryGirl.create(:rating, submission_id: min_submission.id, comment_id: first_comment.id)
+    
+
+    visit submission_path(min_submission)
+    expect(page).to have_content(first_rating.troll)
+    expect(page).to have_content(first_rating.funny)
+    expect(page).to have_content(first_rating.story)
+    expect(page).to have_content(first_rating.helpful)
   end
 end
