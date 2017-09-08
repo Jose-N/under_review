7f85192fdbbfd8147d759fc4cdb9050f03c7cae3
Jose Naylor <jose.c.naylor@gmail.com>
2017-09-08 16:25:13 -0400

    added model test for submission_keyword, made sure overall ratings dont show if there are no ratings, added test to see if overall ratings shows up on show page

--- app/models/submission_keyword.rb
+++ app/models/submission_keyword.rb
@@ -1,4 +1,7 @@
 class SubmissionKeyword < ApplicationRecord
   belongs_to :submission
   belongs_to :keyword
+
+  validates :submission_id, presence: true
+  validates :keyword_id, presence: true
 end
