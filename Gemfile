c023c972f46c4da238d6d514c56830960420ae3e
Jose Naylor <jose.c.naylor@gmail.com>
2017-09-12 16:46:19 -0400

    added react_on_rails

--- Gemfile
+++ Gemfile
@@ -44,3 +44,5 @@ group :production do
 end
 
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem 'mini_racer', platforms: :ruby
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -27,6 +27,10 @@ require 'rspec/rails'
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  # Ensure that if we are running js tests, we are using latest webpack assets
+  # This will use the defaults of :js and :server_rendering meta tags
+  ReactOnRails::TestHelper.configure_rspec_to_compile_assets(config)
+
   config.include Capybara::DSL
   config.include Warden::Test::Helpers
   
@@ -72,5 +76,9 @@ require "capybara/rails"
 require "valid_attribute"
 
 RSpec.configure do |config|
+  # Ensure that if we are running js tests, we are using latest webpack assets
+  # This will use the defaults of :js and :server_rendering meta tags
+  ReactOnRails::TestHelper.configure_rspec_to_compile_assets(config)
+
   config.include FactoryGirl::Syntax::Methods
 end
