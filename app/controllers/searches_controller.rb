c785d10967adb2ed5cc7d8a03da495e3befc23b3
Kristen Lynne Jordan <kristlynne@Kristens-MacBook-Pro.local>
2017-09-13 14:36:15 -0400

    Built spec and removed extraneous files

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,11 +4,18 @@ class SearchesController < ApplicationController
   end
 
   def result
-    if params[:search]
+    if params[:search] && params[:search] != ''
       @results = []
       @results.concat(Submission.where('title ilike ?',  "%#{params[:search]}%"))
       @results.concat(Submission.where('description ilike ?', "%#{params[:search]}"))
       @results = @results.uniq
+      if @results == []
+        redirect_to searches_path,
+        notice: "Sorry, we couldn't find this jawn."
+      end
+    else
+      redirect_to searches_path,
+      notice: 'Sorry, please enter a valid search.'
     end
   end
 end
