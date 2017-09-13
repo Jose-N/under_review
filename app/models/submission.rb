918681fde1a5d2c3b7719c7ed40e88361dfbdf7e
Kristen Lynne Jordan <kristlynne@Kristens-MacBook-Pro.local>
2017-09-13 11:58:32 -0400

    Added screenshot and avatar photo sizing limits

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -7,7 +7,7 @@ class Submission < ApplicationRecord
 
   validates :title, presence: true
   validates :user_id, presence: true
-  validates :screenshot, presence: true
+  validates :screenshot, presence: true, file_size: {maximum: 1.0.megabytes.to_i}
   validates :troll, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5}
   validates :funny, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5}
   validates :helpful, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5}
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+require 'file_size_validator'
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -9,6 +11,6 @@ class User < ApplicationRecord
   validates_presence_of :last_name
   validates :username, presence: true, uniqueness: true, length: {minimum: 6, maximum: 24}, format: { with: /^\w+$/, multiline: true }
   validates :email, presence: true, format: { with: /\A^([^@\s]+)@((?:[-a-z0-9]+.)+[a-z]{2,})$\z/i, multiline: true }
-
+  validates :avatar, file_size: {maximum: 0.3.megabytes.to_i}
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 end
