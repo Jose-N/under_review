6ac2689137b1ccbca1b58f6774a29804d5ce8da5
Jose Naylor <jose.c.naylor@gmail.com>
2017-09-14 11:12:19 -0400

    finished making submission show page fully react except for comment form, removed controllers that werent being used

--- app/javascript/packs/react-application.js
+++ app/javascript/packs/react-application.js
@@ -1,6 +1,7 @@
 import ReactOnRails from 'react-on-rails';
 
 import IndexPage from '../bundles/Submissions/components/IndexPage';
-import ShowPage from '../bundles/Submissions/components/ShowPage'
+import ShowPage from '../bundles/Submissions/components/ShowPage';
+import SubmissionComments from '../bundles/Submissions/components/SubmissionComments';
 
-ReactOnRails.register({IndexPage, ShowPage})
+ReactOnRails.register({IndexPage, ShowPage, SubmissionComments})
